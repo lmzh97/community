ebad4de732a9c153a8b59463660f664c31eeac16
lmzh97 <zhanglemin97@gmail.com>
2021-09-02 16:27:21 +0800

    add lombok

--- src/main/java/life/silenceradio/community/mapper/UserMapper.java
+++ src/main/java/life/silenceradio/community/mapper/UserMapper.java
@@ -8,7 +8,7 @@ import org.apache.ibatis.annotations.Select;
 @Mapper
 public interface UserMapper {
 
-    @Select("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    @Select("insert into user (name,account_id,token,gmt_create,gmt_modified,avatar) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified},#{avatarUrl})")
     void insert(User user);
 
     @Select("select * from user where token = #{token}")
