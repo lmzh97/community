11540fd05326b387605eb32849fb1d41363d2771
lmzh97 <zhanglemin97@gmail.com>
2021-09-03 17:32:27 +0800

    add interception

--- src/main/java/life/silenceradio/community/controller/IndexController.java
+++ src/main/java/life/silenceradio/community/controller/IndexController.java
@@ -1,29 +1,17 @@
 package life.silenceradio.community.controller;
 
 import life.silenceradio.community.dto.PaginationDTO;
-import life.silenceradio.community.dto.QuestionDTO;
-import life.silenceradio.community.mapper.QuestionMapper;
-import life.silenceradio.community.mapper.UserMapper;
-import life.silenceradio.community.model.Question;
-import life.silenceradio.community.model.User;
 import life.silenceradio.community.service.QuestionService;
-import org.omg.CORBA.PUBLIC_MEMBER;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 @Controller
 public class IndexController {
-
-    @Autowired
-    private UserMapper userMapper;
-
     @Autowired
     private QuestionService questionService;
 
@@ -32,19 +20,7 @@ public class IndexController {
                         Model model,
                         @RequestParam(name = "page", defaultValue = "1") Integer page,
                         @RequestParam(name = "size", defaultValue = "8") Integer size){
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null && cookies.length != 0) {
-            for (Cookie cookie : cookies) {
-                if ("token".equals(cookie.getName())) {
-                    String token = cookie.getValue();
-                    User user = userMapper.findByToken(token);
-                    if (user != null) {
-                        request.getSession().setAttribute("user", user);
-                    }
-                    break;
-                }
-            }
-        }
+
         PaginationDTO pagination = questionService.list(page, size);
         model.addAttribute("pagination", pagination);
         return "index";
--- src/main/java/life/silenceradio/community/controller/ProfileController.java
+++ src/main/java/life/silenceradio/community/controller/ProfileController.java
@@ -1,8 +1,6 @@
 package life.silenceradio.community.controller;
 
 import life.silenceradio.community.dto.PaginationDTO;
-import life.silenceradio.community.mapper.QuestionMapper;
-import life.silenceradio.community.mapper.UserMapper;
 import life.silenceradio.community.model.User;
 import life.silenceradio.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,17 +10,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import java.util.Locale;
 
 
 
 @Controller
 public class ProfileController {
-    @Autowired
-    private UserMapper userMapper;
-
     @Autowired
     private QuestionService questionService;
 
@@ -32,20 +25,7 @@ public class ProfileController {
                           @RequestParam(name = "size", defaultValue = "8") Integer size,
                           HttpServletRequest request,
                           Model model) {
-        User user = null;
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null && cookies.length != 0) {
-            for (Cookie cookie : cookies) {
-                if ("token".equals(cookie.getName())) {
-                    String token = cookie.getValue();
-                    user = userMapper.findByToken(token);
-                    if (user != null) {
-                        request.getSession().setAttribute("user", user);
-                    }
-                    break;
-                }
-            }
-        }
+        User user = (User) request.getSession().getAttribute("user");
 
         if (user == null) {
             return "redirect:/";
